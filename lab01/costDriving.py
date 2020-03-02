fd8ad8bd2131bdfb74e98e04fbcc9757d017e91e
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-02 22:13:52 +0100

    Update costDriving.py

--- lab01/costDriving.py
+++ lab01/costDriving.py
@@ -1,3 +1,7 @@
+""" Write a program that prompts the user to enter the distance to drive,
+the fuel efficiency of the car in liters per 100km, and the price per liter, and displays the
+cost of the trip. """
+
 distance = input("Enter the driving distance: ")
 liters = input("Enter liters per 100 km: ")
 price = input("Enter price per liter (LEK): ")
