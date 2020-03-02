0d8ccdb8879b945e2356bdce77ace07b9c6ec173
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-02 22:15:14 +0100

    Update fahrenheit.py

--- lab01/fahrenheit.py
+++ lab01/fahrenheit.py
@@ -1,3 +1,9 @@
+""" Write a program that reads a Celsius degree in a float value from the
+console, then converts it to Fahrenheit and displays the result. The formula for the
+conversion is as follows:
+
+fahrenheit = (9 / 5) * celsius + 32 """
+
 celcius = input("Enter a degree in Celcius: ");
 fahrenheit = (9.0/5.0) * float(celcius) + 32.0
 print(str(fahrenheit) + " Fahrenheit")
