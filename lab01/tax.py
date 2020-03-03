8a4525c2e618e4bf69becdd9efdd05d65a4a82df
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-03 12:23:25 +0100

    Delete tax.py

--- lab01/tax.py
+++
@@ -1,5 +0,0 @@
-""" Write a program that reads the purchase amount and calculates the sales tax
-(6%). Display the tax with three digits after the decimal point. """
-
-purchase = input("Enter the purchase amount: ")
-print("Sales tax is " + str(float(purchase) * 6.0 / 100.0) + " LEK")
