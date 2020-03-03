fb045a57214642c36be3da78efe912adffba358d
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-03 12:23:07 +0100

    Delete circle.py

--- lab01/circle.py
+++
@@ -1,8 +0,0 @@
-""" Write a program that displays the area and perimeter of a circle that has a
-radius of 5.5 using the following formula: (Hint: “import math” for “pi”at first) """
-
-import math
-
-radius = 5.5
-print ("The perimeter is: " + str(2 * radius * math.pi))
-print("The area is: " + str(radius ** 2 * math.pi))
