2b5f9688e8aad1d6b0cadddd717733e2428b420a
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-03-03 12:23:13 +0100

    Delete costDriving.py

--- lab01/costDriving.py
+++
@@ -1,8 +0,0 @@
-""" Write a program that prompts the user to enter the distance to drive,
-the fuel efficiency of the car in liters per 100km, and the price per liter, and displays the
-cost of the trip. """
-
-distance = input("Enter the driving distance: ")
-liters = input("Enter liters per 100 km: ")
-price = input("Enter price per liter (LEK): ")
-print("The cost of driving is " + str((float(distance) / 100.0) * float(liters) * float(price)) + " LEK")
