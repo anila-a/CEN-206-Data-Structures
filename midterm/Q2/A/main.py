b8d578e10566330e631b3c3d43c64e495ac5f66b
Anila Hoxha <51361033+anila-a@users.noreply.github.com>
2020-05-16 14:54:38 +0200

    Update main.py

--- midterm/Q2/A/main.py
+++ midterm/Q2/A/main.py
@@ -7,16 +7,7 @@ Write a singlyQueue class, using a singly linked list as storage. Test your clas
 by adding, removing several elements. Submit, singlyQueue.py and testing.py.
 '''
 
-from singlyQueue import *
-
-def display(queue):
-    first = queue._head # Start from the head node
-    if queue.is_empty(): # Check if the queue is empty
-        return 'The queue is empty' # If so, return this statement
-    while first is not None: # If not, for each element in the queue, print its value
-        print(first._element, '-> ', end = "") # Print the value of the current node
-        first = first._next # Go to the next node
-    print(end = "None") # At the end, print None
+from display import *
 
 l = SinglyQueue() # Create an instance of the SinglyQueue class
 
